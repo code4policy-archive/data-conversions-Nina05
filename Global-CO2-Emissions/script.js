b1c90ddd4713f7df4ea04f48c3388d8057320371
Andreína Mesalles <andreinamesalles@gmail.com>
2022-01-15 12:39:33 -0500

    Test initial chart

--- Global-CO2-Emissions/script.js
+++ Global-CO2-Emissions/script.js
@@ -31,7 +31,7 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("Global-Emissions.csv", function(error, data) {
+d3.tsv("dummy.tsv", function(error, data) {
   if (error) throw error;
 
   data.forEach(function(d) {
